b0676e739b345e459c1a5dd3870def916873108d
tiagorafael98 <tiago-rafael_98@hotmail.com>
2019-07-01 15:48:50 +0100

    Criação do controller 'UtilizadoresController" e respetivas Views.

---
+++ TekkenGame/TekkenGame/Controllers/UtilizadoresController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using TekkenGame.Models;
+
+namespace TekkenGame.Controllers
+{
+    public class UtilizadoresController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Utilizadores
+        public ActionResult Index()
+        {
+            return View(db.Utilizadores.ToList());
+        }
+
+        // GET: Utilizadores/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return RedirectToAction("Index");
+            }
+            Utilizadores utilizadores = db.Utilizadores.Find(id);
+            if (utilizadores == null)
+            {
+                return RedirectToAction("Index");
+            }
+            return View(utilizadores);
+        }
+
+        // GET: Utilizadores/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Utilizadores/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,UserName,NomeCompleto,Email,ContactoTelefonico,DataNascimento")] Utilizadores utilizadores)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Utilizadores.Add(utilizadores);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(utilizadores);
+        }
+
+        // GET: Utilizadores/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Utilizadores utilizadores = db.Utilizadores.Find(id);
+            if (utilizadores == null)
+            {
+                return RedirectToAction("Index");
+            }
+            return View(utilizadores);
+        }
+
+        // POST: Utilizadores/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,UserName,NomeCompleto,Email,ContactoTelefonico,DataNascimento")] Utilizadores utilizadores)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(utilizadores).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(utilizadores);
+        }
+
+        // GET: Utilizadores/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return RedirectToAction("Index");
+            }
+            Utilizadores utilizadores = db.Utilizadores.Find(id);
+            if (utilizadores == null)
+            {
+                return RedirectToAction("Index");
+            }
+            return View(utilizadores);
+        }
+
+        // POST: Utilizadores/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Utilizadores utilizadores = db.Utilizadores.Find(id);
+            db.Utilizadores.Remove(utilizadores);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
